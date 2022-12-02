6d41cb5df0da6e2ac990a9b8a4077c93c843375d
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2022-12-02 19:04:12 +0200

    Inttial SetUp

--- sketch_dec2b/sketch_dec2b.ino
+++ sketch_dec2b/sketch_dec2b.ino
@@ -1,3 +1,18 @@
+#include <TM1637.h>
+
+#include <DHT.h>
+#include <DHT_U.h>
+int CLK = 2;
+int DIO = 3;
+
+TM1637 tm1637(2, 3);
+TM1637 tm(CLK, DIO);
+
+#define DHTPIN A0
+#define DHTTYPE DHT11 
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
   // put your setup code here, to run once:
 
