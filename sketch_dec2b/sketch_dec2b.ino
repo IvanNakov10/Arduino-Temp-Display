10c8f003418803c8b50edc7474a01acee12f185c
Nakov <77243772+IvanNakov10@users.noreply.github.com>
2023-02-01 00:07:27 +0200

    ad

--- sketch_dec2b/sketch_dec2b.ino
+++ sketch_dec2b/sketch_dec2b.ino
@@ -2,14 +2,15 @@
 
 #include <DHT.h>
 #include <DHT_U.h>
+
+#define DHTPIN A0
+#define DHTTYPE DHT11 
+
 int CLK = 2;
 int DIO = 3;
 
 TM1637 tm1637(2, 3);
 TM1637 tm(CLK, DIO);
-
-#define DHTPIN A0
-#define DHTTYPE DHT11 
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -24,7 +25,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(10000);
+  delay(1000);
   float Tempreture = dht.readTemperature();
   if (isnan(Tempreture)) {
   Serial.println("Error: Failed to read temp");
